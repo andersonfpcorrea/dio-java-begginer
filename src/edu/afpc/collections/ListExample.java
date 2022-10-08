ec1e6b1e7bc27a0efaf679c859a38ba14e11b6bd
Anderson Correa <andersonfpcorrea@gmail.com>
2022-10-08 13:49:50 -0300

    java collections - lists

--- src/edu/afpc/collections/ListExample.java
+++ src/edu/afpc/collections/ListExample.java
@@ -1,13 +1,16 @@
 package edu.afpc.collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ListExample {
     public static void main(String[] args) {
-        ArrayList<Double> grades = new ArrayList<Double>();
+        ArrayList<Double> grades = new ArrayList<>();
         grades.add(7d);
         System.out.println(grades);
         System.out.println(grades.indexOf(7d));
+
+
         grades.add(2.0);
         grades.add(2.0);
         grades.add(2.0);
@@ -20,6 +23,8 @@ public class ListExample {
         grades.set(grades.indexOf(5d), 6d);
         System.out.println(grades);
         System.out.println(grades.contains(10d));
+        System.out.println(grades.get(1)); // 2.0
+        System.out.println("The lowest grade: " + Collections.min(grades));
 
 
     }
